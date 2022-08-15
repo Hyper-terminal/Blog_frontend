d3c8086591b7cccb62c5c0a85bf27325629a4149
Hyper-terminal <abhisharma001001001@gmail.com>
2022-08-15 19:57:34 +0530

    3 files modified, 1 deleted

--- src/user/signin/Signin.jsx
+++ src/user/signin/Signin.jsx
@@ -8,120 +8,120 @@ import Stack from '@mui/material/Stack';
 import { Navigate } from 'react-router-dom';
 
 const Signin = () => {
-    const [loginDetails, setLoginDetails] = React.useState({
-        email: '',
-        password: '',
+  const [loginDetails, setLoginDetails] = React.useState({
+    email: '',
+    password: '',
+    error: '',
+    redirectToReferer: false,
+  });
+
+  const handleInput = (e) => {
+    const { name, value } = e.target;
+    setLoginDetails((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
         error: '',
-        redirectToReferer: false,
+      };
     });
+  };
 
-    const handleInput = (e) => {
-        const { name, value } = e.target;
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    const { email, password } = loginDetails;
+    const user = { email, password };
+
+    signin(user).then((data) => {
+      if (data.error) {
         setLoginDetails((prevValue) => {
-            return {
-                ...prevValue,
-                [name]: value,
-                error: '',
-            };
+          return {
+            ...prevValue,
+            error: data.error,
+          };
         });
-    };
-
-    const handleClick = (e) => {
-        e.preventDefault();
-
-        const { email, password } = loginDetails;
-        const user = { email, password };
-
-        signin(user).then((data) => {
-            if (data.error) {
-                setLoginDetails((prevValue) => {
-                    return {
-                        ...prevValue,
-                        error: data.error,
-                    };
-                });
-            } else {
-                // authenticate
-                authenticate(data, () => {
-                    setLoginDetails({ email: '', password: '', redirectToReferer: true });
-                });
-            }
+      } else {
+        // authenticate
+        authenticate(data, () => {
+          setLoginDetails({ email: '', password: '', redirectToReferer: true });
         });
-    };
+      }
+    });
+  };
 
-    const authenticate = (jwt, next) => {
-        if (window !== 'undefined') {
-            localStorage.setItem('jwt', JSON.stringify(jwt));
-            next();
-        }
-    };
-
-    const signin = (user) => {
-        return fetch('http://localhost:8080/api/v1/signin', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(user),
-        })
-            .then((res) => {
-                return res.json();
-            })
-            .catch((err) => console.log(err));
-    };
-
-    if (loginDetails.redirectToReferer) {
-        return <Navigate to="/" replace />;
+  const authenticate = (jwt, next) => {
+    if (window !== 'undefined') {
+      localStorage.setItem('jwt', JSON.stringify(jwt));
+      next();
     }
+  };
 
-    return (
-        <div className="flex_container">
-            <div className="signup_box">
-                <h1 className="signin_title">Sign In</h1>
+  const signin = (user) => {
+    return fetch('http://localhost:8080/api/v1/signin', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => console.log(err));
+  };
 
-                <Stack sx={{ width: '100%' }} spacing={2}>
-                    <Alert
-                        sx={{ display: loginDetails.error ? '' : 'none' }}
-                        severity="error"
-                    >
-                        {loginDetails.error}
-                    </Alert>
-                </Stack>
+  if (loginDetails.redirectToReferer) {
+    return <Navigate to="/" replace />;
+  }
 
-                <form>
-                    <TextField
-                        onChange={handleInput}
-                        value={loginDetails.email}
-                        name="email"
-                        fullWidth
-                        id="outlined-basic"
-                        label="Email"
-                        variant="outlined"
-                        type="email"
-                        margin="normal"
-                    />
-                    <TextField
-                        onChange={handleInput}
-                        value={loginDetails.password}
-                        name="password"
-                        fullWidth
-                        margin="normal"
-                        id="outlined-basic"
-                        label="Password"
-                        variant="outlined"
-                        type="password"
-                    />
-                    <div className="fab">
-                        <Fab variant="extended" onClick={handleClick}>
-                            <LoginIcon sx={{ mr: 1 }} />
-                            Sign in
-                        </Fab>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex_container">
+      <div className="signup_box">
+        <h1 className="signin_title">Sign In</h1>
+
+        <Stack sx={{ width: '100%' }} spacing={2}>
+          <Alert
+            sx={{ display: loginDetails.error ? '' : 'none' }}
+            severity="error"
+          >
+            {loginDetails.error}
+          </Alert>
+        </Stack>
+
+        <form>
+          <TextField
+            onChange={handleInput}
+            value={loginDetails.email}
+            name="email"
+            fullWidth
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            type="email"
+            margin="normal"
+          />
+          <TextField
+            onChange={handleInput}
+            value={loginDetails.password}
+            name="password"
+            fullWidth
+            margin="normal"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+            type="password"
+          />
+          <div className="fab">
+            <Fab variant="extended" onClick={handleClick}>
+              <LoginIcon sx={{ mr: 1 }} />
+              Sign in
+            </Fab>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
