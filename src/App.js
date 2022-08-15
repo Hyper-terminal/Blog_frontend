d0b33caca3fb2df3b04b69dca4df75bb3df055ac
Hyper-terminal <abhisharma001001001@gmail.com>
2022-08-15 16:03:14 +0530

    added routing mechanism

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './style.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './user/signup/Signup';
 import Signin from './user/signin/Signin';
+import Home from "./Home";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="signup" element={<Signup />} />
-          <Route path="signin" element={<Signin />} />
-        </Route>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/signin" element={<Signin />} />
+        <Route exact path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
