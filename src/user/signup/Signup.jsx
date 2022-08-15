1b1db5f5835b09e5b9e56614eb68ee1ead7e5cac
Hyper-terminal <abhisharma001001001@gmail.com>
2022-08-15 12:45:24 +0530

    2 files modified

--- src/user/signup/Signup.jsx
+++ src/user/signup/Signup.jsx
@@ -28,6 +28,17 @@ const Signup = () => {
     const { name, email, password } = userDetails;
     const user = { name, email, password };
     console.log(user);
+
+    fetch('http://localhost:8080/signup', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user)
+    })
+    .then(res => return res.json())
+    .catch(err => console.log(err));
   };
 
   return (
