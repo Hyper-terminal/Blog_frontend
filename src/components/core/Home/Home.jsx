a4bcea735f95d7e18e30d60eeabfe6271a6f85d1
Hyper-terminal <abhisharma001001001@gmail.com>
2022-08-16 15:23:24 +0530

    mobile navbar

--- src/components/core/Home/Home.jsx
+++ src/components/core/Home/Home.jsx
@@ -3,7 +3,16 @@ import React from "react";
 
 const Home = () => {
     return (
+        <div>
+        <h1>Home page anbdjsh</h1>
+        <p>fdhsjkfhgfjksdfksdf</p>
+        <p>fdhsjkfhgfjksdfksdf</p>
+        <p>fdhsjkfhgfjksdfksdf</p>
+        <p>fdhsjkfhgfjksdfksdf</p>
         <h1>Home page</h1>
+        <h1>Home page</h1>
+        <h1>Home page</h1><h1>Home page</h1><h1>Home page</h1><h1>Home page</h1><h1>Home page</h1><h1>Home page</h1><h1>Home page</h1><h1>Home page</h1><h1>Home page</h1><h1>Home page</h1>
+        </div>
     )
 };
 
--- src/components/core/Navbar/Navbar.jsx
+++ src/components/core/Navbar/Navbar.jsx
@@ -1,89 +1,88 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-import "./Navbar.scss";
-import { HiMenuAlt4, HiX } from "react-icons/hi";
-import { motion } from "framer-motion";
+import { Link } from 'react-router-dom';
+import './Navbar.scss';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
 const Navbar = () => {
-
-  const [toggle, setToggle] = React.useState(false)
-
-
+  const [toggle, setToggle] = React.useState(false);
 
   return (
     <nav>
-      <div className='app__navbar'>
-        <div >
-          <ul className='app__navbar-links'>
-
-
-            <li className='app__flex ' key='link-home'>
+      <div className="app__navbar">
+        <div>
+          <ul className="app__navbar-links">
+            <li className="app__flex " key="link-home">
               <div />
-              <Link className='a' to="home">Home</Link>
+              <Link className="desktop_link" to="home">
+                Home
+              </Link>
             </li>
 
-
-            <li className='app__flex ' key='link-signin'>
+            <li className="app__flex " key="link-signin">
               <div />
-              <Link className='a' to="signin">Sign In</Link>
+              <Link className="desktop_link" to="signin">
+                Sign In
+              </Link>
             </li>
 
-
-            <li className='app__flex ' key='link-signup'>
+            <li className="app__flex " key="link-signup">
               <div />
-              <Link className='a' to='signup'>Sign Up</Link>
+              <Link className="desktop_link" to="signup">
+                Sign Up
+              </Link>
             </li>
           </ul>
         </div>
       </div>
 
       {/* mobile menu */}
-      <div className='app__navbar-mobile'>
-        <div className='app__navbar-menu'>
-          <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-          {toggle && (
-            <motion.div
-              whileInView={{ x: [300, 0] }}
-              transition={{ duration: 0.85, ease: "easeOut" }}
-            >
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-              <ul className='app__navbar-links'>
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: 'easeOut' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul className="app__navbar-mobilelinks">
+              <li className="mobile_li" key="home">
+                <Link
+                  onClick={() => setToggle(false)}
+                  className="mobile_link"
+                  to="home"
+                >
+                  Home
+                </Link>
+              </li>
 
+              <li className="mobile_li" key="signin">
+                <Link
+                  onClick={() => setToggle(false)}
+                  className="mobile_link"
+                  to="signin"
+                >
+                  Sign In
+                </Link>
+              </li>
 
-                <li key='home'>
-                  <div />
-                  <Link onClick={() => setToggle(false)} className='a' to="home">Home</Link>
-                </li>
-
-
-                <li key='signin'>
-                  <div />
-                  <Link onClick={() => setToggle(false)} className='a' to="signin">Sign In</Link>
-                </li>
-
-
-                <li key='signup'>
-                  <div />
-                  <Link onClick={() => setToggle(false)} className='a' to='signup'>Sign Up</Link>
-                </li>
-              </ul>
-
-              <HiX onClick={() => setToggle(false)} />
-
-            </motion.div>
-          )}
-
-        </div>
+              <li className="mobile_li" key="signup">
+                <Link
+                  onClick={() => setToggle(false)}
+                  className="mobile_link"
+                  to="signup"
+                >
+                  Sign Up
+                </Link>
+              </li>
+            </ul>
+          </motion.div>
+        )}
       </div>
-
-
-
-    </nav >
-
-
-
-  )
-}
+    </nav>
+  );
+};
 
 export default Navbar;
