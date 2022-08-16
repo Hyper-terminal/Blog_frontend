43004fb704a3a3bbdce90cda8f4940978de43064
Hyper-terminal <abhisharma001001001@gmail.com>
2022-08-16 17:48:41 +0530

    navbar completed

--- src/components/core/Navbar/Navbar.jsx
+++ src/components/core/Navbar/Navbar.jsx
@@ -37,49 +37,50 @@ const Navbar = () => {
       </div>
 
       {/* mobile menu */}
+      <div style={{ display: 'flex', background: '#edf2f8' }}>
+        <div className="app__navbar-menu">
+          <HiMenuAlt4 onClick={() => setToggle(true)} />
 
-      <div className="app__navbar-menu">
-        <HiMenuAlt4 onClick={() => setToggle(true)} />
+          {toggle && (
+            <motion.div
+              whileInView={{ x: [300, 0] }}
+              transition={{ duration: 0.85, ease: 'easeOut' }}
+            >
+              <HiX onClick={() => setToggle(false)} />
+              <ul className="app__navbar-mobilelinks">
+                <li className="mobile_li" key="home">
+                  <Link
+                    onClick={() => setToggle(false)}
+                    className="mobile_link"
+                    to="/"
+                  >
+                    Home
+                  </Link>
+                </li>
 
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul className="app__navbar-mobilelinks">
-              <li className="mobile_li" key="home">
-                <Link
-                  onClick={() => setToggle(false)}
-                  className="mobile_link"
-                  to="/"
-                >
-                  Home
-                </Link>
-              </li>
+                <li className="mobile_li" key="signin">
+                  <Link
+                    onClick={() => setToggle(false)}
+                    className="mobile_link"
+                    to="signin"
+                  >
+                    Sign In
+                  </Link>
+                </li>
 
-              <li className="mobile_li" key="signin">
-                <Link
-                  onClick={() => setToggle(false)}
-                  className="mobile_link"
-                  to="signin"
-                >
-                  Sign In
-                </Link>
-              </li>
-
-              <li className="mobile_li" key="signup">
-                <Link
-                  onClick={() => setToggle(false)}
-                  className="mobile_link"
-                  to="signup"
-                >
-                  Sign Up
-                </Link>
-              </li>
-            </ul>
-          </motion.div>
-        )}
+                <li className="mobile_li" key="signup">
+                  <Link
+                    onClick={() => setToggle(false)}
+                    className="mobile_link"
+                    to="signup"
+                  >
+                    Sign Up
+                  </Link>
+                </li>
+              </ul>
+            </motion.div>
+          )}
+        </div>
       </div>
     </nav>
   );
