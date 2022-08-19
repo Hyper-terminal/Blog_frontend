dc20865e83b95927aaf34e86db48fc58ddcf4bd3
Hyper-terminal <abhisharma001001001@gmail.com>
2022-08-19 22:30:16 +0530

    modified:   src/components/user/profile/Profile.jsx

--- src/components/user/profile/Profile.jsx
+++ src/components/user/profile/Profile.jsx
@@ -1,14 +1,11 @@
 import React from 'react'
-<<<<<<< HEAD
 import { Navigate, useParams } from 'react-router-dom'
 import { isAuthenticated } from '../../auth';
-=======
 import { Link, useParams } from 'react-router-dom'
 import { isAuthenticated } from '../../auth';
 import { read } from '../apiUrl';
 import defaultProfile from "../../../assets/avatar.png";
 import Delete from '../delete profile/Delete';
->>>>>>> af92f61 (	new file:   src/components/user/delete profile/Delete.jsx)
 
 const Profile = () => {
     const { userId } = useParams();
@@ -17,7 +14,6 @@ const Profile = () => {
         redirectToSignin: false
     });
 
-<<<<<<< HEAD
     React.useEffect(() => {
         fetch(`/user/${userId}`, {
             method: 'GET',
@@ -27,51 +23,45 @@ const Profile = () => {
                 Authorization: `Bearer ${isAuthenticated().token}`
             }
         }).then(res => res.json())
-=======
 
-    React.useEffect(() => {
-        read(userId, isAuthenticated().token)
->>>>>>> af92f61 (	new file:   src/components/user/delete profile/Delete.jsx)
-            .then(data => {
-                if (data.error) {
-                    setUserDetails({ redirectToSignin: true })
-                } else {
-                    setUserDetails({ user: data });
+
+        React.useEffect(() => {
+            read(userId, isAuthenticated().token)
+                .then(data => {
+                    if (data.error) {
+                        setUserDetails({ redirectToSignin: true })
+                    } else {
+                        setUserDetails({ user: data });
+                    }
+                })
+                .catch(err => console.log(err));
+        }, [userId])
+
+        if (userDetails.redirectToSignin) {
+            return <Navigate to='signin' replace />
+        }
+
+        return (
+            <div style={{ textAlign: 'center' }}>
+                <h1>Profile</h1>
+
+                <img src={defaultProfile} height="100" width="100" alt="Profile Image" />
+                <p>Name: {userDetails.user.name}</p>
+                <p>Email: {userDetails.user.email}</p>
+                <p>Joined: {new Date(userDetails.user.created).toDateString()}</p>
+
+
+                {/* edit and delete button */}
+                {
+                    isAuthenticated().user._id == userDetails.user._id &&
+                    <>
+                        <Link to='user/edit'>Edit</Link>
+                        <Delete userid={userDetails.user._id} />
+                    </>
                 }
-            })
-            .catch(err => console.log(err));
-    }, [userId])
 
-    if (userDetails.redirectToSignin) {
-        return <Navigate to='signin' replace />
+            </div >
+        )
     }
-
-    return (
-        <div style={{ textAlign: 'center' }}>
-            <h1>Profile</h1>
-<<<<<<< HEAD
-            <p>Name: {isAuthenticated().user.name}</p>
-            <p>Email: {isAuthenticated().user.email}</p>
-=======
-            <img src={defaultProfile} height="100" width="100" alt="Profile Image" />
-            <p>Name: {userDetails.user.name}</p>
-            <p>Email: {userDetails.user.email}</p>
-            <p>Joined: {new Date(userDetails.user.created).toDateString()}</p>
->>>>>>> af92f61 (	new file:   src/components/user/delete profile/Delete.jsx)
-
-
-
-    {/* edit and delete button */ }
-    {
-        isAuthenticated().user._id == userDetails.user._id &&
-        <>
-            <Link to='user/edit'>Edit</Link>
-            <Delete userid={userDetails.user._id} />
-        </>
-    }
-
-        </div >
-    )
-}
 
 export default Profile
